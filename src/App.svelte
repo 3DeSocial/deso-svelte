4d6f06ca30b6cdf548e02ac9808b7ed0f2ecf3a0
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-06-02 13:32:55 +0100

    Implemented:
    Get like status
    Get diamond status
    Like / Unlike
    Send level one diamond

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
 
-	import {identity,  configure, updateLikeStatus, getLikesForPost, getDiamondsForPost } from 'deso-protocol';
+	import {identity,  configure, updateLikeStatus, getLikesForPost, getDiamondsForPost, getSinglePost, sendDiamonds } from 'deso-protocol';
 	import { onMount } from 'svelte';	
 	import { writable } from 'svelte/store';
 
 	let userStore = writable(null);
 	let loggedIn = false;
 	let publicKey =null;
-	let testPost = 'd4c4b7a26a7fdedb83cdb4902815f9bc82047c87381fa2027820b22604fe09c6';
+	let testPost = 'aaebf5cdc74423356f53ce865ed88b0ee41bba8c9d496e86b9e32c3135fa140a';
 	let reply = '';
+	let responseMessage = null;
 	onMount ((count) => {
 		configure({
 			spendingLimitOptions: {
-			// NOTE: this value is in Deso nanos, so 1 Deso * 1e9
-			GlobalDESOLimit: 1 * 1e9, // 1 Deso
-			// Map of transaction type to the number of times this derived key is
-			// allowed to perform this operation on behalf of the owner public key
-			TransactionCountLimitMap: {
-			BASIC_TRANSFER: 100, // 2 basic transfer transactions are authorized
-			SUBMIT_POST: 10,
-			LIKE: 100,
-		//	UNLIKE: 100,
-			FOLLOW: 100
-		//	UNFOLLOW: 100			
+				// NOTE: this value is in Deso nanos, so 1 Deso * 1e9
+				GlobalDESOLimit: 1 * 1e9, // 1 Deso
+				// Map of transaction type to the number of times this derived key is
+				// allowed to perform this operation on behalf of the owner public key
+				TransactionCountLimitMap: {
+				BASIC_TRANSFER: 100, // 2 basic transfer transactions are authorized
+				SUBMIT_POST: 10,
+				LIKE: 100,
+				FOLLOW: 100
+				}
 			}
-		}
 		})		
 		identity.subscribe((state) => {
 		const event = state.event;
@@ -46,38 +45,58 @@
 			break;			
 
 		}
-		//{(testPost)?testPost:''
-		//{(publicKey)?publicKey:''}
+
 		});
 	});
 
+	const sendLikeHandler = async () =>{
+
+		// get param values
+		let postHashHex = testPost;
+		let readerPublicKeyBase58Check = publicKey;
+
+
+		sendLike(readerPublicKeyBase58Check, testPost)
+
+	}
+
+	const userHasLikedHandler = async () =>{
+		let myPublicKey = publicKey;
+		let postHashHex = testPost;
+
+		let likedStatus = await userHasLiked(myPublicKey,postHashHex);
+		responseMessage = 'Your key '+myPublicKey+' has liked post '+postHashHex;
+	}
+
+	const sendDiamondHandler = async () =>{
+		// get param values
+		let postHashHex = testPost;
+		let readerPublicKeyBase58Check = publicKey;
+		sendDiamond(readerPublicKeyBase58Check, postHashHex);
+	}
+
+	const getLikesHandler = async () =>{
+
+	}
+
+	const getDiamondsForPostHandler = () =>{
+
+		responseMessage = 'Your key '+myPublicKey+' has diamonded post '+postHashHex;
+
+	}
+
+	const sendReplyHandler = () =>{
+
+	}
 	const userHasLiked = async (myPublicKey, passedPostHash) =>{
 
-		let keyToCheck = publicKey;
-		if(myPublicKey){
-			keyToCheck = myPublicKey
-		}
-
-		let postHashHex = testPost;
-		if(passedPostHash){
-			postHashHex = passedPostHash;
-		};		
-		let res = await getLikes(postHashHex);
+		let res = await getLikes(passedPostHash);
 		let likers = res.Likers;
-		console.log(likers);
-
-		let result = likers.some(profile => profile.PublicKeyBase58Check === keyToCheck);
+		let result = likers.some(profile => profile.PublicKeyBase58Check === myPublicKey);
 		return result;
 	}
 
-	const sendLike=async()=>{
-
-		// Define the postHashHex and readerPublicKeyBase58Check parameters
-		let postHashHex = testPost;
-		let readerPublicKeyBase58Check = publicKey;
-		const params = {
-			PostHashHex: testPost
-		};
+	const sendLike=async(readerPublicKeyBase58Check, postHashHex)=>{
 
 		let liked = await userHasLiked(readerPublicKeyBase58Check, postHashHex);
 		let isUnlike=false;
@@ -87,8 +106,8 @@
 		};
 
 		const likeParams = {
-			LikedPostHashHex: testPost,
-			ReaderPublicKeyBase58Check:publicKey,
+			LikedPostHashHex: postHashHex,
+			ReaderPublicKeyBase58Check:readerPublicKeyBase58Check,
 			IsUnlike: isUnlike
 		};
 		const options = {
@@ -98,10 +117,13 @@
 		updateLikeStatus(likeParams, options)
 		.then(response => {
 			console.log(response);
+			responseMessage = JSON.stringify(response);
 		})
 		.catch(error => {
 			console.log('error recieved...')
 			console.error(error);
+			responseMessage = JSON.stringify(error);
+
 		});
 
 	}
@@ -124,36 +146,75 @@
 
 
 
-	const userHasDiamonded = () =>{
 
+	const sendDiamond=async(readerPublicKeyBase58Check, passedPostHash)=>{
+		let dimondStaus = await userHasDiamonded(readerPublicKeyBase58Check, passedPostHash);
+		if(dimondStaus){
+			console.log('You have sent diamonds to this post');
+			console.log(dimondStaus);
+		} else {
+			console.log('You have not sent diamonds to this post');
+			console.log(dimondStaus);		
+			const receiverPublicKey = await getPostAuthor(passedPostHash);
+			const senderPublicKey = readerPublicKeyBase58Check;
+			const diamondLevel = 1; // Diamond level can be from 1 to 6
+
+			const params = {
+				SenderPublicKeyBase58Check: senderPublicKey,
+				ReceiverPublicKeyBase58Check: receiverPublicKey,
+				DiamondLevel: diamondLevel,
+				DiamondPostHashHex: passedPostHash
+			};
+
+			console.log('params',params);
+			
+			sendDiamonds(params).then((response) => {
+				console.log(response);
+				responseMessage = JSON.stringify(response);
+
+			}).catch(error => {
+				console.error(error);
+				responseMessage = JSON.stringify(error);
+
+			});
+		}
 	}
 
-	const sendDiamond=async()=>{
-		console.log(`sendDiamond testPost: ${testPost}, publicKey: ${publicKey}`);
+	const getPostAuthor = async(passedPostHashHex) =>{
+		const params = {PostHashHex:passedPostHashHex};
+		let res = await getSinglePost(params);
+		return res.PostFound.ProfileEntryResponse.PublicKeyBase58Check;
+	}
+
+	const getDiamonds  = async (passedPostHash) =>{
+
 		const params = {
-			PostHashHex: testPost
+			PostHashHex: passedPostHash,
+			Limit: 100000,
+			Offset: 0
 		};
-		getDiamondsForPost(params)
-		.then(diamonds => console.log(diamonds))
-		.catch(error => console.error(error));
+
+		// Call the getLikesForPost function
+		return getDiamondsForPost(params)
+	}
+
+	const userHasDiamonded = async (myPublicKey, passedPostHash) =>{
+		let res = await getDiamonds(passedPostHash);
+		let diamondSenders = res.DiamondSenders;
+		let result = diamondSenders.some(profile => profile.PublicKeyBase58Check === myPublicKey);
+		return result;
+	}
+
+	const sendReply = async()=>{
 
 	}
 
-	const sendReply=async()=>{
-		console.log(`testPost: ${testPost}, publicKey: ${publicKey}`);
-	}
+	const sendReplyOnce = async()=>{
+
+	}	
 
 	const handleLoginClick=async()=>{
 		await identity.login();
-		
-	/*	await identity.requestPermissions({
-    TransactionCountLimitMap: {
-      SUBMIT_POST: 10,
-	  LIKE: 1000,
-	  FOLLOW: 1000
-    },
-  });*/
-	
 	}
 
 	const handleLogoutClick=async()=>{
@@ -162,18 +223,23 @@
 </script>
 <main>
 	{#if !loggedIn}	
-	<h2>Login</h2>
-	<div>
-	<button id="login" on:click={handleLoginClick}>Login With DeSo</button>	
-	</div>
+		<h2>Login</h2>
+		<div>
+		<button id="login" on:click={handleLoginClick}>Login With DeSo</button>	
+		</div>
 	{:else}	
 	<div>
-	<h2>Logout</h2>
-	<button id="login" on:click={handleLogoutClick}>Log out</button>
-	</div>
+		<h2>Logout</h2>
+		<button id="login" on:click={handleLogoutClick}>Log out</button>
+		</div>
 	{/if}
+	{#if responseMessage!=null}	
+		<section>
+			<div>{responseMessage}</div>
+		</section>
+	{/if}	
 	<section>
-	<form id="like-form" on:submit|preventDefault={sendLike}>
+	<form id="like-form" on:submit|preventDefault={sendLikeHandler}>
 		<h2>Send Like</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex" bind:value="{testPost}"/>
@@ -181,7 +247,7 @@
 		<input type="text" name="publicKey"  bind:value="{publicKey}"/>
 		<button type="submit" id="send-like">Like</button>
 	</form>
-	<form id="diamond-form" on:submit|preventDefault={sendDiamond}>
+	<form id="diamond-form" on:submit|preventDefault={sendDiamondHandler}>
 		<h2>Send Diamond</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"   bind:value="{testPost}"/>
@@ -189,7 +255,7 @@
 		<input type="text" name="publicKey"  bind:value="{publicKey}"/>
 		<button type="submit"  id="send-diamond">Diamond</button>
 	</form>	
-	<form id="reply-form" on:submit|preventDefault={sendReply}>
+	<form id="reply-form" on:submit|preventDefault={sendReplyHandler}>
 		<h2>Send reply</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
@@ -201,19 +267,19 @@
 	</form>
 	</section>
 	<section>
-	<form id="get-likes-form" on:submit|preventDefault={getLikes}>
+	<form id="get-likes-form" on:submit|preventDefault={getLikesHandler}>
 		<h2>Get Likes For Post</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
 		<button  type="submit"  id="get-likes">Get Likes</button>
 	</form>		
-	<form id="has-liked-form" on:submit|preventDefault={userHasLiked}>
+	<form id="has-liked-form" on:submit|preventDefault={userHasLikedHandler}>
 		<h2>Have I liked This Post?</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
 		<button  type="submit"  id="get-likes">Get Likes</button>
 	</form>		
-	<form id="get-diamonds-form" on:submit|preventDefault={sendReply}>
+	<form id="get-diamonds-form" on:submit|preventDefault={getDiamondsForPostHandler}>
 		<h2>Get Diamonds For Post</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
@@ -221,13 +287,13 @@
 		<input type="text" name="publicKey"  bind:value="{publicKey}"/>
 		<button  type="submit"  id="get-diamonds">Get Diamonds</button>
 	</form>
-	<form id="has-diamonded-form" on:submit|preventDefault={getLikes}>
+	<form id="has-diamonded-form" on:submit|preventDefault={getLikesHandler}>
 		<h2>Have I liked This Post?</h2>
 		<label>postHashHex</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
 		<button  type="submit"  id="get-likes">Get Likes</button>
 	</form>		
-	<form id="get-blogs" on:submit|preventDefault={sendReply}>
+	<form id="get-blogs" on:submit|preventDefault={sendReplyHandler}>
 		<h2>Get Blog Posts For User</h2>
 		<label>userPk</label>
 		<input type="text" name="postHashHex"  bind:value="{testPost}"/>
